f8c0205daa75463ba337904494460599e34a0604
Demola <ariyaademola@gmail.com>
2021-07-22 15:41:44 +0100

    chore: remove empty components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
 import { ProductList } from './components/ProductList';
-import { SocialFeed } from './components/SocialFeed';
 
 function App() {
     return (
         <ChakraProvider>
-            <Header />
             <ProductList />
-            <SocialFeed />
-            <Footer />
         </ChakraProvider>
     );
 }
